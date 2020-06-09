ae79661adb04fd78a85535546638598a3e8ee1d0
malditokoala <edgargomezborboa@hotmail.com>
2020-06-08 21:57:22 -0700

    Adding Custom Hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import imagen from "./cryptomonedas.png";
+import Formulario from "./components/Formulario";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -39,6 +40,7 @@ function App() {
       </div>
       <div>
         <Heading>Cotiza Cryptomonedas al instante</Heading>
+        <Formulario />
       </div>
     </Contenedor>
   );
